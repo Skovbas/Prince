77238ad6589300fd83dce02e300ebc1ba40f0342
Skovbas <ksmua29@gmail.com>
2023-06-18 18:28:23 +0200

    Added menu

--- code/main.py
+++ code/main.py
@@ -2,26 +2,90 @@ import pygame, sys
 from settings import * 
 from level import Level
 from game_data import level_0
+from menu import Button
 
-# Pygame setup 
+# Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((screen_width,screen_height))
+
+# Set up the screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Game Menu")
+
+background_image = pygame.image.load("./graphics/menu/back.png").convert()
+background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 pygame.display.set_caption("Prince of Persia")
+
 clock = pygame.time.Clock()
 level = Level(level_0,screen)
 
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				pygame.quit()
-				sys.exit()
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+# Create a button
+levels_button = Button("Levels", "./graphics/fonts/GARMNDB.ttf", 50, screen_width // 2 - 55, screen_height // 2)
+play_button = Button("Play", "./graphics/fonts/GARMNDB.ttf", 50, screen_width // 2 - 40, screen_height // 2 + 100)
+exit_button = Button("Exit", "./graphics/fonts/GARMNDB.ttf", 50, screen_width // 2 - 40, screen_height // 2 + 200)
 
-	screen.fill('black')
-	level.run()
- 
-	pygame.display.update()
-	clock.tick(60)
+def game():
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					pygame.quit()
+					sys.exit()
+			elif event.type == pygame.QUIT:
+				pygame.quit() 
+				sys.exit()
+
+		screen.fill('black')
+		level.run()
+
+		clock.tick(60)
+		
+		pygame.display.update()
+
+
+# Game menu function
+def game_menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        # Update buttons
+        levels_button.update()
+        play_button.update()
+        exit_button.update()
+
+        if pygame.mouse.get_pressed()[0]:
+            if levels_button.is_hovered:
+                # Handle levels button click
+                print("Levels button clicked!")
+                # Replace the print statement with your levels logic
+
+            elif play_button.is_hovered:
+                # Handle play button click
+                game()
+                # Replace the print statement with your play logic
+
+            elif exit_button.is_hovered:
+                # Exit the menu
+                pygame.quit()
+                sys.exit()
+                
+        screen.blit(background_image, (0, 0))
+        levels_button.render(screen)
+        play_button.render(screen)
+        exit_button.render(screen)
+    
+        # Render the button
+        levels_button.render(screen)
+        play_button.render(screen)
+        exit_button.render(screen)
+
+        pygame.display.update()
+
+# Run the game menu
+game_menu()
--- code/tempCodeRunnerFile.py
+++ code/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-tile_list = import_cut_graphics('./graphics/terrain/wall.png')
-                        # tile_surface = terrain_tile_list[int(val)]
+)
